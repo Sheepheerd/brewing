dba1f84cbb10ade6515199048e5122f0d28e413f
Shepard Garrett <shepard@heerd.dev>
2025-11-03 18:46:57 -0500

    feat: understanding case statements. very similar to vhdl

--- src/d1/lib/d1.ex
+++ src/d1/lib/d1.ex
@@ -4,15 +4,30 @@ defmodule D1 do
   """
 
   @doc """
-  Hello world.
+  CLI Temperature Converter
 
   ## Examples
 
-      iex> D1.hello()
-      :world
+      iex> D1.f_to_c("32")
+      0.0
 
   """
-  def hello do
-    :world
+  def f_to_c(temp_str) do
+    str = String.trim(temp_str)
+
+    case Integer.parse(str) do
+      {int, ""} ->
+        convert(int)
+
+      _ ->
+        case Float.parse(str) do
+          {float, ""} -> convert(float)
+          _ -> IO.puts("Invalid number")
+        end
+    end
+  end
+
+  def convert(f) do
+    (f - 32) * 5 / 9
   end
 end
--- src/d1/test/d1_test.exs
+++ src/d1/test/d1_test.exs
@@ -2,7 +2,7 @@ defmodule D1Test do
   use ExUnit.Case
   doctest D1
 
-  test "greets the world" do
-    assert D1.hello() == :world
+  test "Converting Temps" do
+    assert D1.f_to_c("32") == 0
   end
 end
