15880675dabe6cb5b498e73056916d2f70eaf3e9
Shepard Garrett <shepard@heerd.dev>
2025-11-04 14:21:06 -0500

    fix: adding improvements into to d2

--- src/d2/lib/d2.ex
+++ src/d2/lib/d2.ex
@@ -20,3 +20,16 @@ IO.puts("index #{index}")
 
 IO.puts("x: #{rem(index, size_x)}")
 IO.puts("y: #{floor(index / size_x)}")
+
+# Improvements
+
+coords =
+  Enum.with_index(map)
+  |> Enum.find_value(fn {row, y} ->
+    case Enum.find_index(row, &(&1 == "+")) do
+      nil -> false
+      x -> {x, y}
+    end
+  end)
+
+IO.inspect(coords, label: "Found + at")
